6b015ae26fbaa8d79039f88f1ecad041805444e2
Demian Rodriguez <demian85@gmail.com>
2016-08-15 18:49:20 -0300

    correctly minify js source

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+const webpack = require('webpack');
+
 module.exports = {
     entry: "./src/main.js",
     output: {
         path: __dirname,
         filename: "app.bundle.js"
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 };
