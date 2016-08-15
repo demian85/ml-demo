82611eddb38c2244c3cd8acbf21a60e1c24fa4ee
Demian Rodriguez <demian85@gmail.com>
2016-08-15 17:41:24 -0300

    Basic carousel implementation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 function init() {
     history.replaceState(null, null, '#product-description');
+    initCarousel();
+}
+
+function initCarousel() {
+    const slider = document.querySelector('#carousel-bar');
+    document.querySelector('#carousel .left-arrow').addEventListener('click', function(e) {
+        const current = slider.offsetLeft;
+        if (current < 0) {
+            slider.style.left = current + 100 + 'px';
+        }
+    });
+    document.querySelector('#carousel .right-arrow').addEventListener('click', function(e) {
+        const current = slider.offsetLeft;
+        slider.style.left = current - 100 + 'px';
+    });
 }
 
 window.addEventListener('DOMContentLoaded', init);
